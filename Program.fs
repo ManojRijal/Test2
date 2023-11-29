f128279bf63e74c5f607577983c4cbbef884a99e
Manoj Rijal <manojsmacbook@Manojs-MacBook-Air.local>
2023-11-29 08:36:25 -0500

    First comment

---
+++ Program.fs
@@ -0,0 +1,19 @@
+﻿type Genre =
+    | Horror
+    | Drama
+    | Thriller
+    | Comedy
+    | Fantasy
+    | Sport
+
+type Director = {
+    Name: string
+    Movies: int
+}
+type Movie = {
+    Name: string
+    RunLength: int
+    Genre: Genre
+    Director: Director
+    IMDBRating: float
+}
